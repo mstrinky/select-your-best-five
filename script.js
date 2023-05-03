67f6b9eddb61df5ba1ed6a52ce6aa289f2893ca4
Mst Rinky Akter <mstrinkya@gmail.com>
2023-05-03 22:10:20 +0600

    calculate option complete

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// console.log('linked');
+console.log('linked');
 document.getElementById('calculate').addEventListener('click',function(){
     // console.log('clicked');
     const perPlayer = document.getElementById('player');
@@ -34,47 +34,6 @@ const totalField = total.innerText;
 total.innerText = calculateTotal;
 })
 
-document.getElementById('messi').addEventListener('click',function(){
-  // console.log('clicked');
-  const messi = document.getElementById('messi-text');
-  const messiElement = messi.innerText;
-  // console.log(messiElement);
-  const firstText = document.getElementById('first-text');
-firstText.innerText = messiElement;
-})
 
-document.getElementById('neymar').addEventListener('click',function(){
-  const neymar = document.getElementById('neymar-text');
-const neymarElement = neymar.innerText;
 
-const secondText  = document.getElementById('second-text');
-secondText.innerText = neymarElement;
-})
-document.getElementById('mbappe').addEventListener('click',function(){
-  const mbappe = document.getElementById('mbappe-text');
-const mbappeElement = mbappe.innerText;
 
-const thirdText  = document.getElementById('third-text');
-thirdText.innerText = mbappeElement;
-})
-document.getElementById('machado').addEventListener('click',function(){
-  const machado = document.getElementById('machado-text');
-const machadoElement = machado.innerText;
-
-const forthText  = document.getElementById('forth-text');
-forthText.innerText = machadoElement;
-})
-document.getElementById('ramos').addEventListener('click',function(){
-  const ramos = document.getElementById('ramos-text');
-const ramosElement =ramos.innerText;
-
-const fifthText  = document.getElementById('fifth-text');
-fifthText.innerText = ramosElement;
-})
-document.getElementById('sanches').addEventListener('click',function(){
-  const sanches= document.getElementById('sanches-text');
-const sanchesElement =sanches.innerText;
-
-const sixthText  = document.getElementById('sixth-text');
-sixthText.innerText = sanchesElement;
-})
