25d3f9038fef071f134193dbba4ec86425370388
Mst Rinky Akter <mstrinkya@gmail.com>
2023-05-06 20:37:00 +0600

    add length

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ document.getElementById('calculate').addEventListener('click',function(){
     // console.log(player);
     const playerExpenses = document.getElementById('player-expenses');
     const playerExpensesField = playerExpenses.innerText;
-    playerExpenses.innerText = player * 5;
+    playerExpenses.innerText = player * cardArray.length;
     // console.log(playerExpensesField);
 })
 
