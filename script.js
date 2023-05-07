78094bcd099c3654353005a251dc68271c56559b
Mst Rinky Akter <mstrinkya@gmail.com>
2023-05-07 23:25:58 +0600

    add navbar

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-// console.log('linked');
-
 const cardArray=[];
 
 function display(cardProduct){
-// console.log(cardProduct);
-const tableBody = document.getElementById('card-product');
+  const tableBody = document.getElementById('card-product');
 tableBody.innerHTML = '';
 for(let i=0; i<cardProduct.length; i++){
-  // console.log(cardArray[i]);
   const name = cardArray[i].playerName;
   const tr = document.createElement('tr');
   tr.innerHTML =`
@@ -20,28 +16,22 @@ for(let i=0; i<cardProduct.length; i++){
 }
 }
 function selection(element){
-  // console.log(element.parentNode.parentNode.children[0]);
   const playerName = element.parentNode.parentNode.children[0].innerText;
-  // console.log(playerName);
-  const playerObj = {
+    const playerObj = {
     playerName:playerName
   }
   cardArray.push(playerObj);
-  // console.log(cardArray);
-  // document.getElementById('player').value= cardArray.length;
   display(cardArray);
 }
 
 document.getElementById('calculate').addEventListener('click',function(){
-    // console.log('clicked');
     const perPlayer = document.getElementById('player');
     const perPlayerString = perPlayer.value ;
     const player = parseInt(perPlayerString);
-    // console.log(player);
     const playerExpenses = document.getElementById('player-expenses');
     const playerExpensesField = playerExpenses.innerText;
     playerExpenses.innerText = player * cardArray.length;
-    // console.log(playerExpensesField);
+    
 })
 
 
@@ -61,10 +51,9 @@ document.getElementById('calculate-total').addEventListener('click',function(){
   const playerNumber = parseInt(playerString);
 
   const calculateTotal = playerNumber + coach + manager;
-//   console.log(calculateTotal);
-const total = document.getElementById('total');
-const totalField = total.innerText;
-total.innerText = calculateTotal;
+  const total = document.getElementById('total');
+  const totalField = total.innerText;
+  total.innerText = calculateTotal;
 })
 
 
